6f0c0e3d987e5281e9f8d9c086a90a7549708a41
Ruben <rivasrubene97@gmail.com>
2025-05-10 12:26:22 -0600

    Added Authentication endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\AuthController;
 
 Route::apiResource('users', UserController::class);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->get('/profile', [AuthController::class, 'profile']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
