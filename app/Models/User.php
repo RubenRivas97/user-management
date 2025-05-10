8b1ae69512663a8312862679f65954d7a9b76e11
Ruben <rivasrubene97@gmail.com>
2025-05-10 05:11:07 -0600

    Added Profile and Role entities with relationships to User

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,15 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function profile()
+    {
+        return $this->hasOne(Profile::class);
+    }
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
 }
